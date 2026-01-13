247ffb74574818f44eab12f901993635ae3d7b5c
Darshan Gowda <96766940+darshansrc@users.noreply.github.com>
2026-01-14 02:14:03 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,33 @@
 import { Brain } from "lucide-react";
 import { InfraBedrock } from "./_components/infra-bedrock";
 import Header from "./_components/header";
+import {
+  IconBold,
+  IconBolt,
+  IconChartAreaLine,
+  IconCpu2,
+} from "@tabler/icons-react";
 
 const DashedArrow = ({ className = "" }: { className?: string }) => (
   <div className={`flex items-center absolute z-0 ${className}`}>
-    <div className="grow border-t-2 border-dashed border-gray-400 h-0"></div>
+    <div
+      className="grow h-px"
+      style={{
+        backgroundImage:
+          "linear-gradient(to right, #9ca3af 50%, transparent 50%)",
+        backgroundSize: "10px 1px",
+        backgroundRepeat: "repeat-x",
+        animation: "movingDash 0.6s linear infinite",
+      }}
+    />
     <div className="w-0 h-0 border-t-[6px] border-t-transparent border-b-[6px] border-b-transparent border-l-[8px] border-l-gray-400" />
+    <style jsx>{`
+      @keyframes movingDash {
+        to {
+          background-position: 10px 0;
+        }
+      }
+    `}</style>
   </div>
 );
 
@@ -17,7 +39,7 @@ export default function PipelinePage() {
       <Header />
 
       <div className="flex items-center justify-center h-screen ">
-        <div className="max-w-7xl mx-auto border flex items-center justify-center h-150 w-full p-2">
+        <div className="max-w-7xl mx-auto  flex items-center justify-center h-150 w-full p-2">
           <div className="flex flex-row items-center gap-12 h-full">
             {/* Column 1: Sources */}
             <div className="flex flex-col gap-12 relative">
@@ -40,10 +62,10 @@ export default function PipelinePage() {
 
             {/* Column 2: Processing */}
             <div className="flex flex-col gap-8 relative">
-              <div className="p-2 border flex items-center justify-center flex-col rounded-lg bg-white z-10 relative">
+              <div className="p-2 h-36 w-36 border flex items-center justify-center flex-col rounded-lg bg-white z-10 relative">
                 <img
                   src="/data-processing.png"
-                  className="size-20"
+                  className="size-16"
                   alt="Processing"
                 />
                 <p className="text-xs text-center font-medium">
@@ -75,22 +97,22 @@ export default function PipelinePage() {
             <div className="flex items-center justify-center relative">
               <div className="relative w-80 h-80 z-10">
                 <div className="absolute top-0 left-0 w-1/2 h-1/2 p-1">
-                  <div className="w-full h-full bg-blue-500 rounded-tl-full flex items-center justify-center text-white font-semibold">
+                  <div className="w-full h-full bg-[#1E293B] rounded-tl-full flex items-center justify-center text-white font-semibold">
                     <p className="text-sm ">Where to Play</p>
                   </div>
                 </div>
                 <div className="absolute top-0 right-0 w-1/2 h-1/2 p-1">
-                  <div className="w-full h-full bg-green-500 rounded-tr-full flex items-center justify-center text-white font-semibold">
+                  <div className="w-full h-full bg-[#B5CAD5] rounded-tr-full flex items-center justify-center text-white font-semibold">
                     <p className="text-sm ">How to Manage</p>
                   </div>
                 </div>
                 <div className="absolute bottom-0 left-0 w-1/2 h-1/2 p-1">
-                  <div className="w-full h-full bg-yellow-500 rounded-bl-full flex items-center justify-center text-white font-semibold">
+                  <div className="w-full h-full  bg-[#B5CAD5] rounded-bl-full flex items-center justify-center text-white font-semibold">
                     <p className="text-sm ">How to Execute</p>
                   </div>
                 </div>
                 <div className="absolute bottom-0 right-0 w-1/2 h-1/2 p-1">
-                  <div className="w-full h-full bg-red-500 rounded-br-full flex items-center justify-center text-white font-semibold">
+                  <div className="w-full h-full bg-[#1E293B]  rounded-br-full flex items-center justify-center text-white font-semibold">
                     <p className="text-sm ">How to Win</p>
                   </div>
                 </div>
@@ -110,12 +132,15 @@ export default function PipelinePage() {
             {/* Column 5: End Points */}
             <div className="flex flex-col items-center gap-8 justify-center relative">
               <div className="h-16 w-64 border rounded flex items-center justify-center bg-white z-10">
+                <IconCpu2 />
                 <p>Komerz Agents</p>
               </div>
               <div className="h-16 w-64 border rounded flex items-center justify-center bg-white z-10">
+                <IconChartAreaLine />
                 <p>Business application and Impact</p>
               </div>
               <div className="h-16 w-64 border rounded flex items-center justify-center bg-white z-10">
+                <IconBolt />
                 <p>ERP and Integrations</p>
               </div>
             </div>
