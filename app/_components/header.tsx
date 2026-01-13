125b8d7a105e13e02f0f97dd6bc4050b54e87742
Darshan Gowda <96766940+darshansrc@users.noreply.github.com>
2026-01-14 03:46:08 +0530

    fix

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,19 +2,30 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="bg-primary fixed top-0 w-screen p-4 ">
-      <div className="max-w-7xl mx-auto flex items-center flex-row gap-2 ">
-        <div className="bg-[#ebe6da] rounded size-8 flex items-center justify-center">
-          <Image src="/icon.svg" alt="Komerz OS Logo" width={20} height={20} />
-        </div>
+    <header className=" fixed top-0 w-screen p-4 ">
+      <div className="max-w-7xl mx-auto flex items-center justify-between w-full flex-row gap-2 ">
         <div>
-          <h1 className="text-md text-white font-semibold">Komerz OS</h1>
+          <h1 className="text-2xl  font-semibold text-black font-mono">
+            Komerz OS
+          </h1>
           <p className="text-xs text-muted-foreground ">
             One of a kind end-to-end operating system to launch and manage brand
             in market across all channels using Al & ML
           </p>
         </div>
+        <div className="">
+          <Image
+            src="/logos/logo-big.svg"
+            alt="Komerz OS Logo"
+            width={48}
+            height={48}
+          />
+        </div>
       </div>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap');
+      </style>
     </header>
   );
 }
